82f9e5855060f1e4ce2773cf034c95ac62d7723e
Yogita <yogimunde@gmail.com>
2021-07-31 21:09:29 +0530

    added f1

--- SampleTestAcc/ViewController.swift
+++ SampleTestAcc/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello Feature 1")
         // Do any additional setup after loading the view.
     }
 
